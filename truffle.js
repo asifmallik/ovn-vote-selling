1d906353282771d4ca8ead75729d49274d702bc9
Asif <asifmallik@4D.(none)>
2018-04-24 18:59:17 +0600

    Add transaction gas limit to 8 million

--- truffle.js
+++ truffle.js
@@ -1,9 +1,10 @@
 module.exports = {
     networks: {
         development: {
-            // gas: 6712390,
+            gas: 8000000,
             host: "localhost",
             port: 8545,
+            accounts: 100,
             network_id: "*" // Match any network id
         }
     }
